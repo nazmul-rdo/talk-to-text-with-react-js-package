0759af96ec320da836b6b48c9c618c1b5ae606b4
nazmul-rdo <mdnazmulkhanrdo@gmail.com>
2023-12-27 08:58:11 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import './App.css';
 
 function App() {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <p>Microphone: {listening ? 'on' : 'off'}</p>
+        <button className="button" onClick={SpeechRecognition.startListening}>Start</button>
+        <button className="button" onClick={SpeechRecognition.stopListening}>Stop</button>
+        <button className="button" onClick={resetTranscript}>Reset</button>
+        <p className="transcript">{transcript}</p>
+      </div>
     </div>
   );
 }
